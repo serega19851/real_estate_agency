d1bb977938885d77d54266cbfbd3499be0fc6633
Сергей Антонов <antonovseantonoff@yandex.ru>
2023-01-16 00:03:24 +0300

    Fixed admin models

---
+++ property/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from .models import Flat, Complaint, Owner
+
+
+class OwnerInline(admin.TabularInline):
+    model = Owner.apartments.through
+    raw_id_fields = ['owner']
+
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address',)
+    readonly_fields = ('created_at',)
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+    )
+    list_editable = ('new_building',)
+    list_filter = (
+        'new_building',
+        'rooms_number',
+        'has_balcony',
+    )
+    raw_id_fields = ('likes',)
+
+    inlines = [
+        OwnerInline,
+    ]
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'flat',
+        'text',
+    )
+    raw_id_fields = ('user', 'flat',)
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    raw_id_fields = ('apartments',)
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin)
