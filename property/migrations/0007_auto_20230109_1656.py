841ffb499f37819fa6a52af5460b8e37b4262c6c
Сергей Антонов <antonovseantonoff@yandex.ru>
2023-01-17 11:19:35 +0300

    Fixed related name, blank

--- property/migrations/0007_auto_20230109_1656.py
+++ property/migrations/0007_auto_20230109_1656.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=200, verbose_name='ФИО владельца')),
                 ('phonenumber', models.CharField(max_length=20, verbose_name='Номер владельца')),
                 ('pure_phone', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=20, region=None, verbose_name='Нормализованный номер владельца')),
-                ('apartments', models.ManyToManyField(related_name='apartments_property', to='property.Flat', verbose_name='Квартиры в собственности')),
+                ('apartments', models.ManyToManyField(related_name='owners', to='property.Flat', blank=True, verbose_name='Квартиры в собственности')),
             ],
         ),
     ]
